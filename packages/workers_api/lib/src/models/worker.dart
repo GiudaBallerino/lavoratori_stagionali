4073f60e3cd27ec71f26f07be3e26f636825d505
GiudaBallerino <lorispesarin@gmail.com>
2022-05-29 16:29:48 +0200

    created data models

--- packages/workers_api/lib/src/models/worker.dart
+++ packages/workers_api/lib/src/models/worker.dart
@@ -2,6 +2,7 @@ import 'package:equatable/equatable.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:meta/meta.dart';
 import 'package:uuid/uuid.dart';
+import 'package:workers_api/src/models/submodels/submodels.dart';
 
 import '../../workers_api.dart';
 
@@ -13,22 +14,80 @@ class Worker extends Equatable {
   Worker({
     String? id,
     required this.firstname,
-    required this.lastname
-  }) :assert(id == null || id.isNotEmpty, 'id can not be null and should be empty',), id = id ?? const Uuid().v4();
+    required this.lastname,
+    required this.birthday,
+    required this.birthplace,
+    required this.nationality,
+    required this.address,
+    required this.phone,
+    required this.email,
+    required this.languages,
+    required this.licenses,
+    required this.areas,
+    required this.tasks,
+    required this.experiences,
+    required this.periods,
+    required this.emergencyContacts,
+  })  : assert(
+          id == null || id.isNotEmpty,
+          'id can not be null and should be empty',
+        ),
+        id = id ?? const Uuid().v4();
 
   final String id;
-  String firstname;
-  String lastname;
+  final String firstname;
+  final String lastname;
+  final DateTime birthday;
+  final String birthplace;
+  final String nationality;
+  final String address;
+  final String phone;
+  final String email;
+
+  final List<String> languages;
+  final List<String> licenses;
+  final List<String> areas;
+  final List<String> tasks;
+
+  final List<Experience> experiences;
+  final List<Period> periods;
+  final List<EmergencyContact> emergencyContacts;
 
   Worker copyWith({
     String? id,
     String? firstname,
     String? lastname,
+    DateTime? birthday,
+    String? birthplace,
+    String? nationality,
+    String? address,
+    String? phone,
+    String? email,
+    List<String>? languages,
+    List<String>? licenses,
+    List<String>? areas,
+    List<String>? tasks,
+    List<Experience>? experiences,
+    List<Period>? periods,
+    List<EmergencyContact>? emergencyContacts,
   }) {
     return Worker(
-        id: id ?? this.id,
-        firstname: firstname ?? this.firstname,
-        lastname: lastname ?? this.lastname,
+      id: id ?? this.id,
+      firstname: firstname ?? this.firstname,
+      lastname: lastname ?? this.lastname,
+      birthday: birthday ?? this.birthday,
+      birthplace: birthplace ?? this.birthplace,
+      nationality: nationality ?? this.nationality,
+      address: address ?? this.address,
+      phone: phone ?? this.phone,
+      email: email ?? this.email,
+      languages: languages ?? this.languages,
+      licenses: licenses ?? this.licenses,
+      areas: areas ?? this.areas,
+      tasks: tasks ?? this.tasks,
+      experiences: experiences ?? this.experiences,
+      periods: periods ?? this.periods,
+      emergencyContacts: emergencyContacts ?? this.emergencyContacts,
     );
   }
 
@@ -37,5 +96,22 @@ class Worker extends Equatable {
   JsonMap toJson() => _$WorkerToJson(this);
 
   @override
-  List<Object> get props => [id, firstname, lastname];
+  List<Object> get props => [
+        id,
+        firstname,
+        lastname,
+        birthday,
+        birthplace,
+        nationality,
+        address,
+        phone,
+        email,
+        languages,
+        licenses,
+        areas,
+        tasks,
+        experiences,
+        periods,
+        emergencyContacts,
+      ];
 }
--- packages/workers_api/lib/src/models/worker.g.dart
+++ packages/workers_api/lib/src/models/worker.g.dart
@@ -7,13 +7,47 @@ part of 'worker.dart';
 // **************************************************************************
 
 Worker _$WorkerFromJson(Map<String, dynamic> json) => Worker(
-    id: json['id'] as String,
-    firstname: json['firstname'] as String,
-    lastname: json['lastname'] as String,
-);
+      id: json['id'] as String,
+      firstname: json['firstname'] as String,
+      lastname: json['lastname'] as String,
+      birthday: json['birthday'] as DateTime,
+      birthplace: json['birthplace'] as String,
+      nationality: json['nationality'] as String,
+      address: json['address'] as String,
+      phone: json['phone'] as String,
+      email: json['email'] as String,
+      languages:
+          List.from(json['languages']).map((item) => item as String).toList(),
+      licenses:
+          List.from(json['licenses']).map((item) => item as String).toList(),
+      areas: List.from(json['areas']).map((item) => item as String).toList(),
+      tasks: List.from(json['tasks']).map((item) => item as String).toList(),
+      experiences: List.from(json['experiences'])
+          .map((item) => Experience.fromJson(item))
+          .toList(),
+      periods: List.from(json['periods'])
+          .map((item) => Period.fromJson(item))
+          .toList(),
+      emergencyContacts: List.from(json['emergencyContacts'])
+          .map((item) => EmergencyContact.fromJson(item))
+          .toList(),
+    );
 
 Map<String, dynamic> _$WorkerToJson(Worker instance) => <String, dynamic>{
-  'id': instance.id,
-  'firstname': instance.firstname,
-  'lastname': instance.lastname,
-};
+      'id': instance.id,
+      'firstname': instance.firstname,
+      'lastname': instance.lastname,
+      'birthday': instance.birthday,
+      'birthplace': instance.birthplace,
+      'nationality': instance.nationality,
+      'address': instance.address,
+      'phone': instance.phone,
+      'email': instance.email,
+      'languages': instance.languages,
+      'licenses': instance.licenses,
+      'areas': instance.areas,
+      'tasks': instance.tasks,
+      'experiences':instance.experiences.map((e)=>e.toJson()).toList(),
+      'periods':instance.periods.map((p)=>p.toJson()).toList(),
+      'emergencyContacts':instance.experiences.map((e)=>e.toJson()).toList(),
+    };
