96962419249deeb76ebdd47cb312a87fdd936aa8
GiudaBallerino <lorispesarin@gmail.com>
2022-05-30 13:24:29 +0200

    added tasks, experiences and emercencyContacts implementation for addition and deletion to CreationBloc

--- lib/creation/bloc/creation_bloc.dart
+++ lib/creation/bloc/creation_bloc.dart
@@ -13,7 +13,9 @@ class CreationBloc extends Bloc<CreationEvent, CreationState> {
   CreationBloc({
     required WorkersRepository workersRepository,
   })  : _workersRepository = workersRepository,
-        super(const CreationState(),) {
+        super(
+          const CreationState(),
+        ) {
     on<WorkerSubmitted>(_onWorkerSubmitted);
     on<FirstNameChanged>(_onFirstNameChanged);
     on<LastNameChanged>(_onLastNameChanged);
@@ -31,232 +33,384 @@ class CreationBloc extends Bloc<CreationEvent, CreationState> {
     on<AreaDeleted>(_onAreaDeleted);
     on<PeriodAdded>(_onPeriodAdded);
     on<PeriodDeleted>(_onPeriodDeleted);
+    on<TaskAdded>(_onTaskAdded);
+    on<TaskDeleted>(_onTaskDeleted);
+    on<ExperienceAdded>(_onExperienceAdded);
+    on<ExperienceDeleted>(_onExperienceDeleted);
+    on<EmergencyContactAdded>(_onEmergencyContactAdded);
+    on<EmergencyContactDeleted>(_onEmergencyContactDeleted);
   }
 
   final WorkersRepository _workersRepository;
 
   Future<void> _onWorkerSubmitted(
-      WorkerSubmitted event,
-      Emitter<CreationState> emit,
-      ) async {
-    emit(state.copyWith(status: ()=> CreationStatus.loading));
+    WorkerSubmitted event,
+    Emitter<CreationState> emit,
+  ) async {
+    emit(state.copyWith(status: () => CreationStatus.loading));
 
     try {
       await _workersRepository.saveWorker(event.worker);
-      emit(state.copyWith(status: ()=> CreationStatus.success));
+      emit(state.copyWith(status: () => CreationStatus.success));
     } catch (e) {
-      emit(state.copyWith(status: ()=> CreationStatus.failure));
+      emit(state.copyWith(status: () => CreationStatus.failure));
     }
   }
 
   Future<void> _onFirstNameChanged(
-      FirstNameChanged event,
-      Emitter<CreationState> emit,
-      ) async {
-    emit(state.copyWith(status: ()=> CreationStatus.loading));
+    FirstNameChanged event,
+    Emitter<CreationState> emit,
+  ) async {
+    emit(state.copyWith(status: () => CreationStatus.loading));
 
     try {
-      emit(state.copyWith(status: ()=> CreationStatus.success, firstname: ()=> event.text));
+      emit(state.copyWith(
+          status: () => CreationStatus.success, firstname: () => event.text));
     } catch (e) {
-      emit(state.copyWith(status: ()=> CreationStatus.failure));
+      emit(state.copyWith(status: () => CreationStatus.failure));
     }
   }
+
   Future<void> _onLastNameChanged(
-      LastNameChanged event,
-      Emitter<CreationState> emit,
-      ) async {
-    emit(state.copyWith(status: ()=> CreationStatus.loading));
+    LastNameChanged event,
+    Emitter<CreationState> emit,
+  ) async {
+    emit(state.copyWith(status: () => CreationStatus.loading));
 
     try {
-      emit(state.copyWith(status: ()=> CreationStatus.success, lastname: ()=> event.text));
+      emit(state.copyWith(
+          status: () => CreationStatus.success, lastname: () => event.text));
     } catch (e) {
-      emit(state.copyWith(status: ()=> CreationStatus.failure));
+      emit(state.copyWith(status: () => CreationStatus.failure));
     }
   }
+
   Future<void> _onBirthdayChanged(
-      BirthdayChanged event,
-      Emitter<CreationState> emit,
-      ) async {
-    emit(state.copyWith(status: ()=> CreationStatus.loading));
+    BirthdayChanged event,
+    Emitter<CreationState> emit,
+  ) async {
+    emit(state.copyWith(status: () => CreationStatus.loading));
 
     try {
-      DateTime date= DateFormat('dd/MM/yyyy').parse(event.text);
-      emit(state.copyWith(status: ()=> CreationStatus.success, birthday: ()=> date));
+      DateTime date = DateFormat('dd/MM/yyyy').parse(event.text);
+      emit(state.copyWith(
+          status: () => CreationStatus.success, birthday: () => date));
     } catch (e) {
-      emit(state.copyWith(status: ()=> CreationStatus.failure));
+      emit(state.copyWith(status: () => CreationStatus.failure));
     }
   }
+
   Future<void> _onBirthplaceChanged(
-      BirthplaceChanged event,
-      Emitter<CreationState> emit,
-      ) async {
-    emit(state.copyWith(status: ()=> CreationStatus.loading));
+    BirthplaceChanged event,
+    Emitter<CreationState> emit,
+  ) async {
+    emit(state.copyWith(status: () => CreationStatus.loading));
 
     try {
-      emit(state.copyWith(status: ()=> CreationStatus.success, birthplace: ()=> event.text));
+      emit(state.copyWith(
+          status: () => CreationStatus.success, birthplace: () => event.text));
     } catch (e) {
-      emit(state.copyWith(status: ()=> CreationStatus.failure));
+      emit(state.copyWith(status: () => CreationStatus.failure));
     }
   }
+
   Future<void> _onNationalityChanged(
-      NationalityChanged event,
-      Emitter<CreationState> emit,
-      ) async {
-    emit(state.copyWith(status: ()=> CreationStatus.loading));
+    NationalityChanged event,
+    Emitter<CreationState> emit,
+  ) async {
+    emit(state.copyWith(status: () => CreationStatus.loading));
 
     try {
-      emit(state.copyWith(status: ()=> CreationStatus.success, nationality: ()=> event.text));
+      emit(state.copyWith(
+          status: () => CreationStatus.success, nationality: () => event.text));
     } catch (e) {
-      emit(state.copyWith(status: ()=> CreationStatus.failure));
+      emit(state.copyWith(status: () => CreationStatus.failure));
     }
   }
+
   Future<void> _onAddressChanged(
-      AddressChanged event,
-      Emitter<CreationState> emit,
-      ) async {
-    emit(state.copyWith(status: ()=> CreationStatus.loading));
+    AddressChanged event,
+    Emitter<CreationState> emit,
+  ) async {
+    emit(state.copyWith(status: () => CreationStatus.loading));
 
     try {
-      emit(state.copyWith(status: ()=> CreationStatus.success, address: ()=> event.text));
+      emit(state.copyWith(
+          status: () => CreationStatus.success, address: () => event.text));
     } catch (e) {
-      emit(state.copyWith(status: ()=> CreationStatus.failure));
+      emit(state.copyWith(status: () => CreationStatus.failure));
     }
   }
+
   Future<void> _onPhoneChanged(
-      PhoneChanged event,
-      Emitter<CreationState> emit,
-      ) async {
-    emit(state.copyWith(status: ()=> CreationStatus.loading));
+    PhoneChanged event,
+    Emitter<CreationState> emit,
+  ) async {
+    emit(state.copyWith(status: () => CreationStatus.loading));
 
     try {
-      emit(state.copyWith(status: ()=> CreationStatus.success, phone: ()=> event.text));
+      emit(state.copyWith(
+          status: () => CreationStatus.success, phone: () => event.text));
     } catch (e) {
-      emit(state.copyWith(status: ()=> CreationStatus.failure));
+      emit(state.copyWith(status: () => CreationStatus.failure));
     }
   }
+
   Future<void> _onEmailChanged(
-      EmailChanged event,
-      Emitter<CreationState> emit,
-      ) async {
-    emit(state.copyWith(status: ()=> CreationStatus.loading));
+    EmailChanged event,
+    Emitter<CreationState> emit,
+  ) async {
+    emit(state.copyWith(status: () => CreationStatus.loading));
 
     try {
-      emit(state.copyWith(status: ()=> CreationStatus.success, email: ()=> event.text));
+      emit(state.copyWith(
+          status: () => CreationStatus.success, email: () => event.text));
     } catch (e) {
-      emit(state.copyWith(status: ()=> CreationStatus.failure));
+      emit(state.copyWith(status: () => CreationStatus.failure));
     }
   }
 
   Future<void> _onLanguageAdded(
-      LanguageAdded event,
-      Emitter<CreationState> emit,
-      ) async {
-    emit(state.copyWith(status: ()=> CreationStatus.loading));
+    LanguageAdded event,
+    Emitter<CreationState> emit,
+  ) async {
+    emit(state.copyWith(status: () => CreationStatus.loading));
 
     try {
-      emit(state.copyWith(status: ()=> CreationStatus.success, languages: ()=> [...state.languages,...[event.language]]));
+      emit(state.copyWith(
+          status: () => CreationStatus.success,
+          languages: () => [
+                ...state.languages,
+                ...[event.language]
+              ]));
     } catch (e) {
-      emit(state.copyWith(status: ()=> CreationStatus.failure));
+      emit(state.copyWith(status: () => CreationStatus.failure));
     }
   }
 
   Future<void> _onLanguageDeleted(
-      LanguageDeleted event,
-      Emitter<CreationState> emit,
-      ) async {
-    emit(state.copyWith(status: ()=> CreationStatus.loading));
+    LanguageDeleted event,
+    Emitter<CreationState> emit,
+  ) async {
+    emit(state.copyWith(status: () => CreationStatus.loading));
 
     try {
-      List<String> tmp=state.languages;
+      List<String> tmp = state.languages;
       tmp.remove(event.language);
-      emit(state.copyWith(status: ()=> CreationStatus.success, languages: ()=> tmp));
+      emit(state.copyWith(
+          status: () => CreationStatus.success, languages: () => tmp));
     } catch (e) {
-      emit(state.copyWith(status: ()=> CreationStatus.failure));
+      emit(state.copyWith(status: () => CreationStatus.failure));
     }
   }
 
   Future<void> _onLicenseAdded(
-      LicenseAdded event,
-      Emitter<CreationState> emit,
-      ) async {
-    emit(state.copyWith(status: ()=> CreationStatus.loading));
+    LicenseAdded event,
+    Emitter<CreationState> emit,
+  ) async {
+    emit(state.copyWith(status: () => CreationStatus.loading));
 
     try {
-      emit(state.copyWith(status: ()=> CreationStatus.success, licenses: ()=> [...state.licenses,...[event.license]]));
+      emit(state.copyWith(
+          status: () => CreationStatus.success,
+          licenses: () => [
+                ...state.licenses,
+                ...[event.license]
+              ]));
     } catch (e) {
-      emit(state.copyWith(status: ()=> CreationStatus.failure));
+      emit(state.copyWith(status: () => CreationStatus.failure));
     }
   }
 
   Future<void> _onLicenseDeleted(
-      LicenseDeleted event,
-      Emitter<CreationState> emit,
-      ) async {
-    emit(state.copyWith(status: ()=> CreationStatus.loading));
+    LicenseDeleted event,
+    Emitter<CreationState> emit,
+  ) async {
+    emit(state.copyWith(status: () => CreationStatus.loading));
 
     try {
-      List<String> tmp=state.licenses;
+      List<String> tmp = state.licenses;
       tmp.remove(event.license);
-      emit(state.copyWith(status: ()=> CreationStatus.success, licenses: ()=> tmp));
+      emit(state.copyWith(
+          status: () => CreationStatus.success, licenses: () => tmp));
     } catch (e) {
-      emit(state.copyWith(status: ()=> CreationStatus.failure));
+      emit(state.copyWith(status: () => CreationStatus.failure));
     }
   }
 
   Future<void> _onAreaAdded(
-      AreaAdded event,
-      Emitter<CreationState> emit,
-      ) async {
-    emit(state.copyWith(status: ()=> CreationStatus.loading));
+    AreaAdded event,
+    Emitter<CreationState> emit,
+  ) async {
+    emit(state.copyWith(status: () => CreationStatus.loading));
 
     try {
-      emit(state.copyWith(status: ()=> CreationStatus.success, areas: ()=> [...state.areas,...[event.area]]));
+      emit(state.copyWith(
+          status: () => CreationStatus.success,
+          areas: () => [
+                ...state.areas,
+                ...[event.area]
+              ]));
     } catch (e) {
-      emit(state.copyWith(status: ()=> CreationStatus.failure));
+      emit(state.copyWith(status: () => CreationStatus.failure));
     }
   }
 
   Future<void> _onAreaDeleted(
-      AreaDeleted event,
-      Emitter<CreationState> emit,
-      ) async {
-    emit(state.copyWith(status: ()=> CreationStatus.loading));
+    AreaDeleted event,
+    Emitter<CreationState> emit,
+  ) async {
+    emit(state.copyWith(status: () => CreationStatus.loading));
 
     try {
-      List<String> tmp=state.areas;
+      List<String> tmp = state.areas;
       tmp.remove(event.area);
-      emit(state.copyWith(status: ()=> CreationStatus.success, areas: ()=> tmp));
+      emit(state.copyWith(
+          status: () => CreationStatus.success, areas: () => tmp));
     } catch (e) {
-      emit(state.copyWith(status: ()=> CreationStatus.failure));
+      emit(state.copyWith(status: () => CreationStatus.failure));
     }
   }
 
   Future<void> _onPeriodAdded(
-     PeriodAdded event,
-      Emitter<CreationState> emit,
-      ) async {
-    emit(state.copyWith(status: ()=> CreationStatus.loading));
+    PeriodAdded event,
+    Emitter<CreationState> emit,
+  ) async {
+    emit(state.copyWith(status: () => CreationStatus.loading));
 
     try {
       Period period = Period(start: event.start, end: event.end);
-      emit(state.copyWith(status: ()=> CreationStatus.success, periods: ()=> [...state.periods,...[period]]));
+      emit(state.copyWith(
+          status: () => CreationStatus.success,
+          periods: () => [
+                ...state.periods,
+                ...[period]
+              ]));
     } catch (e) {
-      emit(state.copyWith(status: ()=> CreationStatus.failure));
+      emit(state.copyWith(status: () => CreationStatus.failure));
     }
   }
 
   Future<void> _onPeriodDeleted(
-      PeriodDeleted event,
-      Emitter<CreationState> emit,
-      ) async {
-    emit(state.copyWith(status: ()=> CreationStatus.loading));
+    PeriodDeleted event,
+    Emitter<CreationState> emit,
+  ) async {
+    emit(state.copyWith(status: () => CreationStatus.loading));
 
     try {
-      List<Period> tmp=state.periods;
+      List<Period> tmp = state.periods;
       tmp.remove(event.period);
-      emit(state.copyWith(status: ()=> CreationStatus.success, periods: ()=> tmp));
+      emit(state.copyWith(
+          status: () => CreationStatus.success, periods: () => tmp));
     } catch (e) {
-      emit(state.copyWith(status: ()=> CreationStatus.failure));
+      emit(state.copyWith(status: () => CreationStatus.failure));
+    }
+  }
+
+  Future<void> _onTaskAdded(
+    TaskAdded event,
+    Emitter<CreationState> emit,
+  ) async {
+    emit(state.copyWith(status: () => CreationStatus.loading));
+
+    try {
+      emit(state.copyWith(
+          status: () => CreationStatus.success,
+          tasks: () => [
+                ...state.tasks,
+                ...[event.task]
+              ]));
+    } catch (e) {
+      emit(state.copyWith(status: () => CreationStatus.failure));
+    }
+  }
+
+  Future<void> _onTaskDeleted(
+    TaskDeleted event,
+    Emitter<CreationState> emit,
+  ) async {
+    emit(state.copyWith(status: () => CreationStatus.loading));
+
+    try {
+      List<String> tmp = state.tasks;
+      tmp.remove(event.task);
+      emit(state.copyWith(
+          status: () => CreationStatus.success, tasks: () => tmp));
+    } catch (e) {
+      emit(state.copyWith(status: () => CreationStatus.failure));
+    }
+  }
+
+  Future<void> _onExperienceAdded(
+    ExperienceAdded event,
+    Emitter<CreationState> emit,
+  ) async {
+    emit(state.copyWith(status: () => CreationStatus.loading));
+
+    try {
+      emit(state.copyWith(
+          status: () => CreationStatus.success,
+          experiences: () => [
+                ...state.experiences,
+                ...[event.experience]
+              ]));
+    } catch (e) {
+      emit(state.copyWith(status: () => CreationStatus.failure));
+    }
+  }
+
+  Future<void> _onExperienceDeleted(
+    ExperienceDeleted event,
+    Emitter<CreationState> emit,
+  ) async {
+    emit(state.copyWith(status: () => CreationStatus.loading));
+
+    try {
+      List<Experience> tmp = state.experiences;
+      tmp.remove(event.experience);
+      emit(state.copyWith(
+          status: () => CreationStatus.success, experiences: () => tmp));
+    } catch (e) {
+      emit(state.copyWith(status: () => CreationStatus.failure));
+    }
+  }
+
+  Future<void> _onEmergencyContactAdded(
+    EmergencyContactAdded event,
+    Emitter<CreationState> emit,
+  ) async {
+    emit(state.copyWith(status: () => CreationStatus.loading));
+
+    try {
+      EmergencyContact emergencyContact = EmergencyContact(
+          firstname: event.firstname,
+          lastname: event.lastname,
+          phone: event.phone,
+          email: event.email);
+      emit(state.copyWith(
+          status: () => CreationStatus.success,
+          emergencyContacts: () => [
+                ...state.emergencyContacts,
+                ...[emergencyContact]
+              ]));
+    } catch (e) {
+      emit(state.copyWith(status: () => CreationStatus.failure));
+    }
+  }
+
+  Future<void> _onEmergencyContactDeleted(
+      EmergencyContactDeleted event,
+    Emitter<CreationState> emit,
+  ) async {
+    emit(state.copyWith(status: () => CreationStatus.loading));
+
+    try {
+      List<EmergencyContact> tmp = state.emergencyContacts;
+      tmp.remove(event.emergencyContact);
+      emit(state.copyWith(
+          status: () => CreationStatus.success, emergencyContacts: () => tmp));
+    } catch (e) {
+      emit(state.copyWith(status: () => CreationStatus.failure));
     }
   }
 }
