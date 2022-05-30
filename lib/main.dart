8776856f20a66f5ea5d9a3fa07bbda2eb7235850
Davide <49226282+pizidavi@users.noreply.github.com>
2022-05-30 15:27:46 +0200

    Added min window size (#1)

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:window_size/window_size.dart';
 import 'package:storage_workers_api/storage_workers_api.dart';
 
 import 'bootstrap.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    setWindowTitle('Lavoratori Stagionali');
+    setWindowMinSize(const Size(1100, 700));
+    // setWindowMaxSize(Size.infinite);
+  }
 
   final workersApi = StorageWorkersApi(
     plugin: await SharedPreferences.getInstance(),
