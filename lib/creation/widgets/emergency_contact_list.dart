03caa094825ad19d81486d19c5422d7add384563
GiudaBallerino <lorispesarin@gmail.com>
2022-05-30 13:27:28 +0200

    created new widget for emergency contacts

---
+++ lib/creation/widgets/emergency_contact_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EmergencyContactList extends StatelessWidget {
+  const EmergencyContactList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap();
+  }
+}
