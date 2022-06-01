075549360a51ce5d4ff7ee81fcab4819eecdbd3c
GiudaBallerino <lorispesarin@gmail.com>
2022-06-01 17:28:51 +0200

    added configuration file

---
+++ lib/config.dart
@@ -0,0 +1,7 @@
+class Config{
+  static String MONGO_DATABASE = 'lavoratori_stagionali';
+  static List<String> MONGO_COLLECTIONS= ['workers',];
+  static String MONGO_HOST = 'cluster.rhojb.mongodb.net';
+  static String MONGO_USER='lavoratori-stagionali';
+  static String MONGO_PASSWORD='OM1TBvQT5D1rjHB4';
+}
---
+++ lib/config.sample.dart
@@ -0,0 +1,7 @@
+class Config{
+  static String MONGO_DATABASE = '<database>';
+  static List<String> MONGO_COLLECTIONS= ['<collection1>',];
+  static String MONGO_HOST = '<host>';
+  static String MONGO_USER='<username>';
+  static String MONGO_PASSWORD='<password>';
+}
