185e3df100822da65d85adfe6771c457c1797a39
Loris Pesarin <lorispesarin@gmail.com>
2022-06-01 18:05:50 +0200

    Delete config.dart (#10)

--- lib/config.dart
+++
@@ -1,7 +0,0 @@
-class Config{
-  static String MONGO_DATABASE = 'lavoratori_stagionali';
-  static List<String> MONGO_COLLECTIONS= ['workers',];
-  static String MONGO_HOST = 'cluster.rhojb.mongodb.net';
-  static String MONGO_USER='lavoratori-stagionali';
-  static String MONGO_PASSWORD='OM1TBvQT5D1rjHB4';
-}
